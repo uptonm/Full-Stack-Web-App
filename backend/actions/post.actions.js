e6547762e312e0dc63e431b374eab4fea5cb8890
Mike Upton <uptonm@wit.edu>
2018-10-04 13:24:28 -0400

    Added actions for the following routes: - Get all posts - Get one post - Post new post - Edit(put) existing post - Delete existing post - Get Author(User model) of existing post All routes support error handling with http status codes - most edge cases covered, minor tweaks to come

---
+++ backend/actions/post.actions.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const { Post } = require("../models/post");
+const User = require("../models/user");
+
+exports.get = async (req, res) => {
+  // Get all Posts
+  const exists = await Post.find();
+  if (exists.length !== 0) return res.status(200).send(exists);
+  return res.status(400).send("No Posts Found");
+};
+
+exports.getOne = async (req, res) => {
+  // Get Post
+  const exists = await Post.find({ _id: req.params.id });
+  if (exists.length !== 0) {
+    return res.status(200).send(exists);
+  }
+  return res.status(400).send("Post Not Found");
+};
+
+exports.post = async (req, res) => {
+  // Create Post
+  const exists = await Post.find(req.body);
+  if (exists) {
+    // Prevent duplicate posts
+    return res.status(400).send("Post already exists");
+  }
+  const newPost = await new Post(req.body).save();
+  res.status(200).send(newPost);
+};
+
+exports.put = async (req, res) => {
+  // Edit Post
+  const postUpdate = await Post.findByIdAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    (err, res) => {
+      if (err) return res.status(400).send(err);
+    }
+  );
+  return res.status(200).send(postUpdate);
+};
+
+exports.delete = async (req, res) => {
+  // Delete Post
+  const postDelete = await Post.findByIdAndRemove(
+    { _id: req.params.id },
+    (err, res) => {
+      if (err) return res.status(400).send(err);
+    }
+  );
+  return res.status(200).send({ message: `Post ${req.params.id} deleted` });
+};
+
+exports.getAuthor = async (req, res) => {
+  const exists = await Post.findById(req.params.id);
+  if (exists) {
+    const author = await User.findById(exists.author);
+    if (author) {
+      return res.status(200).send(author);
+    }
+    return res.status(400).send("Author not found");
+  }
+  return res.status(400).send("Post not found");
+};
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,10 +1,27 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const { postSchema } = require("./post");
 
 const userSchema = new Schema({
   first: String,
   last: String,
-  email: String
+  email: String,
+  posts: [postSchema]
+});
+
+// Virtual Field, does not persist to database, performs function to calculate locally
+userSchema.virtual("postCount").get(function() {
+  return this.posts.length;
+});
+
+// middleware: run on remove call
+userSchema.pre("remove", function(next) {
+  const Posts = mongoose.model("posts");
+  const Comments = mongoose.model("comments");
+  // On user delete account delete all of their blog posts and comments
+  Posts.deleteMany({ _id: { $in: this.Posts } })
+    .then(() => Comments.deleteMany({ _id: { $in: this.Comments } }))
+    .then(() => next());
 });
 
 const User = mongoose.model("users", userSchema);
