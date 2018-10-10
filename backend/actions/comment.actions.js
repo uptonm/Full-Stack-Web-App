0877c496b05dd792d5d8b6302672f0b3a75470ff
Mike Upton <uptonm@wit.edu>
2018-10-10 09:41:37 -0400

    Added comment get action

--- backend/actions/comment.actions.js
+++ backend/actions/comment.actions.js
@@ -2,6 +2,14 @@ const mongoose = require("mongoose");
 const { Comment } = require("../models/comment");
 const { Post } = require("../models/post");
 
+exports.get = async (req, res) => {
+  const exists = await Comment.findById(req.params.id);
+  if (exists) {
+    return res.status(200).send(exists);
+  }
+  res.status(404).send('Comment not found');
+}
+
 exports.post = async (req, res) => {
   const newComment = await new Comment(req.body).save();
   const update = await Post.findByIdAndUpdate(req.body.post, {
